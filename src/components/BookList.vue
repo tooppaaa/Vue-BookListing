77180418a963a7bbdad9a9e85d7231cb13a39aa7
Dungler, Clément <clement.dungler@enablon.com>
2018-11-21 15:46:24 +0100

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,11 +1,21 @@
 <template>
-<div><h1>{{ title }}</h1></div></template>
+<div>
+    <h1>{{ title }}</h1>
+    <ul>
+    <li v-for="book in books">{{book.title}}, {{book.author}}</li>
+    </ul> 
+</div></template>
 <script>
 export default {
   name: 'BookList',
   data: function() {
     return {
       title: 'All Books',
+      books: [
+        { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+        { title: 'American Gods', author: 'Neil Gaiman' },
+        { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
+      ],
     };
   },
 };
@@ -14,5 +24,14 @@ export default {
 h1,
 h2 {
   font-weight: "normal";
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 18px;
 }
 </style>
